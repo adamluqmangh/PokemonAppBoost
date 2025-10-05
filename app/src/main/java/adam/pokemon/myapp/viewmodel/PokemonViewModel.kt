08d3733671e7606618c4686ee637a8c6f6f5feca
adamluqmangh <adamluqman1611@gmail.com>
2025-10-05 19:48:47 +0800

    add toggle favorite function

--- app/src/main/java/adam/pokemon/myapp/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/adam/pokemon/myapp/viewmodel/PokemonViewModel.kt
@@ -1,9 +1,11 @@
 package adam.pokemon.myapp.viewmodel
 
 import adam.pokemon.myapp.api.pokemonlist.model.ApiResponsePokemonList
+import adam.pokemon.myapp.api.pokemonlist.model.PokemonList
 import adam.pokemon.myapp.api.pokemonlist.repository.PokemonRepository
 import adam.pokemon.myapp.utils.Response
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,9 +20,27 @@ class PokemonViewModel @Inject constructor(
     val pokemonData: LiveData<Response<ApiResponsePokemonList>>
         get() = pokemonRepository.pokemonData
 
+    private val favouriteSet = mutableSetOf<PokemonList>()
+    private val _favourites = MutableLiveData<List<PokemonList>>()
+    val favourites: LiveData<List<PokemonList>> get() = _favourites
+
     init {
         viewModelScope.launch {
             pokemonRepository.getPokemon()
         }
+    }
+
+    fun toggleFavourite(pokemon: PokemonList) {
+        if (favouriteSet.contains(pokemon)) {
+            favouriteSet.remove(pokemon)
+        } else {
+            favouriteSet.add(pokemon)
+        }
+        _favourites.value = favouriteSet.toList()
+    }
+
+
+    fun isFavourite(pokemon: PokemonList): Boolean {
+        return favouriteSet.contains(pokemon)
     }
 }
