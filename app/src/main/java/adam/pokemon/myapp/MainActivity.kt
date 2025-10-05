fe955b707776bb86020dba5a057735592749e3cd
adamluqmangh <adamluqman1611@gmail.com>
2025-10-05 18:26:41 +0800

    add entry point in main activity
    for dagger hilt

--- app/src/main/java/adam/pokemon/myapp/MainActivity.kt
+++ app/src/main/java/adam/pokemon/myapp/MainActivity.kt
@@ -6,8 +6,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
 
