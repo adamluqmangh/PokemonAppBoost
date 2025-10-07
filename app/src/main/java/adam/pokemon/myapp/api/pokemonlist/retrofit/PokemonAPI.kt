d7ba3879c0ccda64a8bc1f1376f563d2240f6b86
adamluqmangh <adamluqman1611@gmail.com>
2025-10-07 21:55:06 +0800

    fetch ability detail from retrofit call

--- app/src/main/java/adam/pokemon/myapp/api/pokemonlist/retrofit/PokemonAPI.kt
+++ app/src/main/java/adam/pokemon/myapp/api/pokemonlist/retrofit/PokemonAPI.kt
@@ -1,11 +1,13 @@
 package adam.pokemon.myapp.api.pokemonlist.retrofit
 
 import adam.pokemon.myapp.api.pokemonlist.model.Ability
+import adam.pokemon.myapp.api.pokemonlist.model.AbilityDetail
 import adam.pokemon.myapp.api.pokemonlist.model.ApiResponsePokemonList
 import adam.pokemon.myapp.api.pokemonlist.model.PokemonAbility
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Url
 
 interface PokemonAPI {
     @GET("api/v2/pokemon")
@@ -17,4 +19,9 @@ interface PokemonAPI {
     suspend fun getPokemonDetail(
         @Path("name") name: String
     ): Response<PokemonAbility>
+
+    @GET
+    suspend fun getAbilityDetail(
+        @Url url: String
+    ): Response<AbilityDetail>
 }
