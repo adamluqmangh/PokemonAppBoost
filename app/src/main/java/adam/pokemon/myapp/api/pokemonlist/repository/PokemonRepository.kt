f360c53b5333e1c09c31c0df1be7350e44f06434
adamluqmangh <adamluqman1611@gmail.com>
2025-10-07 09:06:08 +0800

    post value of pokemon detail in pokemon repository

--- app/src/main/java/adam/pokemon/myapp/api/pokemonlist/repository/PokemonRepository.kt
+++ app/src/main/java/adam/pokemon/myapp/api/pokemonlist/repository/PokemonRepository.kt
@@ -1,6 +1,8 @@
 package adam.pokemon.myapp.api.pokemonlist.repository
 
+import adam.pokemon.myapp.api.pokemonlist.model.Ability
 import adam.pokemon.myapp.api.pokemonlist.model.ApiResponsePokemonList
+import adam.pokemon.myapp.api.pokemonlist.model.PokemonAbility
 import adam.pokemon.myapp.api.pokemonlist.retrofit.PokemonAPI
 import adam.pokemon.myapp.utils.Response
 import androidx.lifecycle.LiveData
@@ -17,20 +19,37 @@ class PokemonRepository  @Inject constructor(
     val pokemonData: LiveData<Response<ApiResponsePokemonList>>
         get() = _pokemonData
 
+    private val _pokemonDetail = MutableLiveData<Response<PokemonAbility>>()
+    val pokemonDetail: LiveData<Response<PokemonAbility>>
+        get() = _pokemonDetail
+
     suspend fun getPokemon() = withContext(Dispatchers.IO){
         _pokemonData.postValue(Response.Loading())
         try{
             val result = pokemonAPI.getPokemon()
             if(result.body() !== null ){
                 _pokemonData.postValue(Response.Success(result.body()))
-                Timber.tag("debug 3").e(result.body().toString())
-
             }else{
                 _pokemonData.postValue(Response.Error(result.errorBody().toString()))
             }
         }catch(e: Exception) {
             _pokemonData.postValue(Response.Error(e.message.toString()))
 
+        }
+    }
+
+
+    suspend fun getPokemonDetail(name: String) = withContext(Dispatchers.IO) {
+        _pokemonDetail.postValue(Response.Loading())
+        try {
+            val result = pokemonAPI.getPokemonDetail(name)
+            if (result.isSuccessful && result.body() !== null) {
+                _pokemonDetail.postValue(Response.Success(result.body()))
+            } else {
+                _pokemonDetail.postValue(Response.Error(result.errorBody().toString()))
+            }
+        } catch (e: Exception) {
+            _pokemonDetail.postValue(Response.Error(e.message.toString()))
         }
     }
 }
